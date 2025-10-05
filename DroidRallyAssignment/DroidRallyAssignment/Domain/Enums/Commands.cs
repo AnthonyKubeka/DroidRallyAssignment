7e9bc0818239b48d43cb7ba912f6db0e30239059
AnthonyKubeka <anthonykubeka@gmail.com>
2025-10-05 18:37:23 +0200

    Add Commands and Directions enums, empty tests for validating DroidInput

---
+++ DroidRallyAssignment/DroidRallyAssignment/Domain/Enums/Commands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroidRallyAssignment.Domain.Enums
+{
+    public enum Commands
+    {
+        L = 1,
+        R = 2,
+        M = 3
+    }
+}
--- DroidRallyAssignment/DroidRallyAssignmentTests/DroidRallyTests.cs
+++ DroidRallyAssignment/DroidRallyAssignmentTests/DroidRallyTests.cs
@@ -12,5 +12,33 @@ namespace DroidRallyAssignmentTests
             // Assert
             Assert.True(result);
         }
+
+        [Fact]
+        public void Given_GridDimensions_When_DimensionsAreInValid_Then_ShouldFailAssertAsValid()
+        {
+            // Arrange
+            var gridDimensions = "55";
+            // Act
+            var result = DroidRallyAssignment.Application.DroidInputValidator.IsValidGridDimensions(gridDimensions);
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Given_DroidInput_When_DroidInputValid_Then_ShouldAssertAsValid()
+        {
+            var droidInput = "1 2 N";
+            
+            
+            //Assert.True(result);
+        }
+
+        [Fact]
+        public void Given_DroidInput_When_DroidInputInValid_Then_ShouldFailAssertAsValid()
+        {
+            var droidInput = "12NFF";
+        
+            //Assert.False(result);
+        }
     }
 }
