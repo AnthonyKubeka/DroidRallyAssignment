23e08872169f2ea97c102721c968b04eb589b764
AnthonyKubeka <anthonykubeka@gmail.com>
2025-10-05 18:16:09 +0200

    add DroidInputValidator and first test for IsValidDimensions - failing as function unimplimented yet

---
+++ DroidRallyAssignment/DroidRallyAssignment/Application/DroidInputValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroidRallyAssignment.Application
+{
+    public static class DroidInputValidator
+    {
+        public static bool IsValidGridDimensions(string gridDimensions)
+        {
+            return false; 
+        }
+    }
+}
--- DroidRallyAssignment/DroidRallyAssignmentTests/DroidRallyTests.cs
+++ DroidRallyAssignment/DroidRallyAssignmentTests/DroidRallyTests.cs
@@ -1,11 +1,16 @@
 namespace DroidRallyAssignmentTests
 {
-    public class UnitTest1
+    public class DroidRallyTests
     {
         [Fact]
-        public void Test1()
+        public void Given_GridDimensions_When_DimensionsAreValid_Then_ShouldAssertAsValid()
         {
-
+            // Arrange
+            var gridDimensions = "5 5";
+            // Act
+            var result = DroidRallyAssignment.Application.DroidInputValidator.IsValidGridDimensions(gridDimensions);
+            // Assert
+            Assert.True(result);
         }
     }
 }
