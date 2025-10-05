ef28a961faa28b3260e00eb89d4e5e85c9e1963e
AnthonyKubeka <anthonykubeka@gmail.com>
2025-10-05 19:04:15 +0200

    add Droid class

---
+++ DroidRallyAssignment/DroidRallyAssignment/Domain/Droid.cs
@@ -0,0 +1,31 @@
+﻿using DroidRallyAssignment.Application;
+using DroidRallyAssignment.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroidRallyAssignment.Domain
+{
+    public class Droid
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Directions Direction { get; set; }
+        public static Droid InitialiseDroid(string droidInput)
+        {
+            var inputs = droidInput.Split(' ');
+            var x = int.Parse(inputs[0]);
+            var y = int.Parse(inputs[1]);
+            EnumMapper.TryParseDirection(inputs[2], out var direction);
+
+            return new Droid
+            {
+                X = x,
+                Y = y,
+                Direction = direction
+            };
+        }
+    }
+}
