ca61edfa179528846a4b376790b7af6c9270d3b0
AnthonyKubeka <anthonykubeka@gmail.com>
2025-10-05 18:59:29 +0200

    add initial program logic to validate grid dimensions in console

--- DroidRallyAssignment/DroidRallyAssignment/Program.cs
+++ DroidRallyAssignment/DroidRallyAssignment/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DroidRallyAssignment.Application;
+
 Console.WriteLine("Hello, World!");
 
 /**Brief:
@@ -11,3 +13,15 @@ Console.WriteLine("Hello, World!");
  * displaying the droid's current poisition and direction after each execution, as well as the final position and direction after all commands have been executed.
  * 
  **/
+
+Console.WriteLine("Welcome to the droid navigation module");
+Console.WriteLine("Input the grid's upper-right coordinates and press Enter to begin.");
+Console.WriteLine("Format: X Y (Two positive integers seperated by a single space");
+
+var gridDimensions = Console.ReadLine();
+
+if (!DroidInputValidator.IsValidGridDimensions(gridDimensions))
+{
+    Console.WriteLine("Invalid grid dimensions. Please ensure you enter two positive integers separated by a space.");
+    return;
+}
