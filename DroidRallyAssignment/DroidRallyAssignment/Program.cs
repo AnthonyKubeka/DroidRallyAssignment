fbe1a1ea9361256bcb3d34c770607aa25a70cfba
AnthonyKubeka <anthonykubeka@gmail.com>
2025-10-05 18:08:09 +0200

    Initial setup, test project and requirement summary

---
+++ DroidRallyAssignment/DroidRallyAssignment/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/**Brief:
+ * Droid A has a position (X, Y) and a direction (N, S, E, W)
+ * Grid start coordinates are (0,0) in the bottom left corner
+ * Droid commands: turn left 90 degress (L), turn right 90 degrees (R), move forward 1 position in current direction (M)
+ * 
+ * Given the dimensions of the grid, the initial droid position and direction (heading) and a set of commands to execute, 
+ * initialise each droids positoin and direction on the grid and then execute each droids command sequence one at a time,
+ * displaying the droid's current poisition and direction after each execution, as well as the final position and direction after all commands have been executed.
+ * 
+ **/
